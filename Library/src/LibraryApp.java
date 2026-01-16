b0b3f49a84259c3afb4d556c239d8a37dde6dc50
Nahlebnick <kotstepa2021@gmail.com>
2026-01-17 01:33:41 +0300

    Add new book, change price and checkout book functions added

--- Library/src/LibraryApp.java
+++ Library/src/LibraryApp.java
@@ -18,7 +18,13 @@ import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.*;
 
@@ -91,11 +97,13 @@ public class LibraryApp extends JFrame
         JPanel controlPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         
         JButton btnAdd = new JButton("Добавить книгу");
-        btnAdd.addActionListener(e -> addBookDialog());
+        btnAdd.addActionListener(e -> addBook());
         
         JButton btnPrice = new JButton("Переоценка");
+        btnPrice.addActionListener(e -> changePrice());
         
         JButton btnCheckout = new JButton("Выдать книгу");
+        btnCheckout.addActionListener(e -> checkoutBook());
 
         JLabel lblSearch = new JLabel("Поиск:");
         searchField = new JTextField(20);
@@ -123,8 +131,52 @@ public class LibraryApp extends JFrame
         table = new JTable(tableModel);
         add(new JScrollPane(table), BorderLayout.CENTER);
     }
+	
+	private void changePrice() {
+        int selectedRow = table.getSelectedRow();
+        if (selectedRow == -1) {
+            JOptionPane.showMessageDialog(this, "Выберите книгу в таблице!");
+            return;
+        }
 
-	private void addBookDialog()
+        String currentPrice = (String) table.getValueAt(selectedRow, 4);
+        String id = (String) table.getValueAt(selectedRow, 0);
+        String newPrice = JOptionPane.showInputDialog(this, "Текущая цена: " + currentPrice + "\nВведите новую цену:");
+
+        try {
+        	double price = Double.parseDouble(newPrice);
+        } catch (Exception e) {
+        	JOptionPane.showMessageDialog(this, "Invalid price passed!", "Error", JOptionPane.ERROR_MESSAGE);
+        	return;
+        }
+        if (newPrice != null && !newPrice.isEmpty()) {
+            updateBookField(id, "price", newPrice);
+            saveXML();
+            refreshTable();
+        }
+    }
+	
+	private void updateBookField(String id, String tagName, String newValue) {
+        Node bookNode = findBookNodeById(id);
+        if (bookNode != null) {
+            Element elem = (Element) bookNode;
+            Node fieldNode = elem.getElementsByTagName(tagName).item(0);
+            fieldNode.setTextContent(newValue);
+        }
+    }
+	
+	private Node findBookNodeById(String id) {
+        NodeList nList = document.getElementsByTagName("book");
+        for (int i = 0; i < nList.getLength(); i++) {
+            Element elem = (Element) nList.item(i);
+            if (elem.getAttribute("id").equals(id)) {
+                return elem;
+            }
+        }
+        return null;
+    }
+
+	private void addBook()
 	{
 		JTextField titleField = new JTextField();
         JTextField authorField = new JTextField();
@@ -145,8 +197,77 @@ public class LibraryApp extends JFrame
         };
         System.out.print(11);
         int option = JOptionPane.showConfirmDialog(null, message, "Добавить книгу", JOptionPane.OK_CANCEL_OPTION);
+        if (option == JOptionPane.OK_OPTION) {
+            try {
+                Element root = document.getDocumentElement();
+                Element newBook = document.createElement("book");
+
+                newBook.setAttribute("id", idField.getText());
+                newBook.setAttribute("totalCount", totalField.getText());
+                newBook.setAttribute("availableCount", totalField.getText());
+                
+                newBook.appendChild(getElem("title", titleField.getText()));
+                newBook.appendChild(getElem("author", authorField.getText()));
+                newBook.appendChild(getElem("year", yearField.getText()));
+                newBook.appendChild(getElem("price", priceField.getText()));
+                newBook.appendChild(getElem("category", catField.getText()));
+
+                root.appendChild(newBook);
+                saveXML();
+                refreshTable();
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(this, "Ошибка ввода данных");
+            }
+        }
 	}
 	
+	private Element getElem(String tag, String content)
+	{
+		 Element el = document.createElement(tag);
+         el.appendChild(document.createTextNode(content));
+         return el;
+	}
+	
+	private void checkoutBook() {
+        int selectedRow = table.getSelectedRow();
+        if (selectedRow == -1) {
+            JOptionPane.showMessageDialog(this, "Выберите книгу для выдачи!");
+            return;
+        }
+
+        String id = (String) table.getValueAt(selectedRow, 0);
+        Node bookNode = findBookNodeById(id);
+        
+        if (bookNode != null) {
+            Element bookElem = (Element) bookNode;
+            int available = Integer.parseInt(bookElem.getAttribute("availableCount"));
+            
+            if (available > 0) {
+                bookElem.setAttribute("availableCount", String.valueOf(available - 1));
+                saveXML();
+                refreshTable();
+                JOptionPane.showMessageDialog(this, "Книга выдана успешно.");
+            } else {
+                JOptionPane.showMessageDialog(this, "Нет доступных экземпляров!", "Ошибка", JOptionPane.WARNING_MESSAGE);
+            }
+        }
+    }
+	
+	private void saveXML() {
+		 try {
+	            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	            Transformer transformer = transformerFactory.newTransformer();
+	            
+	            //transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	            DOMSource source = new DOMSource(document);
+	            StreamResult result = new StreamResult(new File(XMLFile));
+	            transformer.transform(source, result);
+	        } catch (TransformerException e) {
+	            e.printStackTrace();
+	            JOptionPane.showMessageDialog(this, "Ошибка сохранения файла!");
+	        }		
+	}
+
 	private int loadXML() {
         try 
         {
